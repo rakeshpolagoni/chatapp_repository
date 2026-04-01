c939c549f6b2b02e7796dcf12fe0f623d93cecae
Rakesh Polagoni <rakesh.p@diplsrv.local>
2026-04-01 12:14:35 +0530

    added new feature

--- src/main/java/com/example/chatapp/controller/ChatController.java
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -1,12 +1,19 @@
 package com.example.chatapp.controller;
 
 import com.example.chatapp.entity.ChatMessage;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+	
+	@GetMapping("/")
+    public String home() {
+        return "App is LIVE 🚀";
+    }
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
