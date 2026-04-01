09c8887c736f28282decf43cac5890dc5b57efc7
Rakesh Polagoni <rakesh.p@diplsrv.local>
2026-04-01 12:27:08 +0530

    new change

--- src/main/java/com/example/chatapp/controller/ChatController.java
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -6,8 +6,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class ChatController {
 	
 	@GetMapping("/")
